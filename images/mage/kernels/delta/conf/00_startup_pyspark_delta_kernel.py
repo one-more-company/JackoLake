6fda80e1f668e4ac22060929bc3b72a9c2010df6
Vlado Vladimirov <vlado.vladimirov87@gmail.com>
2025-03-30 21:01:34 +0000

    resolve problem with delta tabels and cration of db

--- images/mage/kernels/delta/conf/00_startup_pyspark_delta_kernel.py
+++ images/mage/kernels/delta/conf/00_startup_pyspark_delta_kernel.py
@@ -19,7 +19,7 @@ spark"""
 spark = SparkSession \\
     .builder \\
     .appName("SparkSession-Delta") \\
-    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \\
+    .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0") \\
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \\
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \\
     .enableHiveSupport() \\
